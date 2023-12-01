a15c395c84a0c5c0dab0c1c289a38e15d172ea9f
abdussalamj <152305032+abdussalamj@users.noreply.github.com>
2023-12-01 22:23:11 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -21,42 +21,6 @@ from babel.numbers import format_currency
 sns.set(style='dark')
 from streamlit.logger import get_logger
 
-byseason_df = pd.read_csv("byseason.csv")
-byseason_df.rename(index={
-    0: "Musim",
-    1: "Spring",
-    2: "Summer",
-    3: "Fall",
-    4: "Winter"
-}, inplace=True)
-byseason_df.rename(columns={
-    "Jumlah Customer.1": "Rata-rata Customer"
-}, inplace=True)
-byseason_df
-
-byweathersit_df = pd.read_csv("byweathersit.csv")
-byweathersit_df.rename(index={
-    0: "Cuaca",
-    1: "Clear",
-    2: "Mist",
-    3: "Light Snow",
-}, inplace=True)
-byweathersit_df.rename(columns={
-    "cnt": "Jumlah Customer",
-    "cnt.1": "Rata-rata Customer"
-}, inplace=True)
-byweathersit_df
-
-byhour_df = pd.read_csv("byhour.csv")
-byhour_df.rename(columns={
-    "casual" : "Casual",
-    "casual.1": "Rata-rata casual",
-    "registered": "Registered",
-    "registered.1": "Rata-rata Registered",
-    "cnt": "Jumlah Customer",
-    "cnt.1": "Rata-rata Customer"
-}, inplace=True)
-byhour_df
 
 st.header('Bike Rental Dashboard 🚲')
 
