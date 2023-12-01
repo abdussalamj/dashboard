d707c45090b5424b01f575031290ef56b8db63a8
abdussalamj <152305032+abdussalamj@users.noreply.github.com>
2023-12-01 22:15:22 +0700

    Update and rename Hello.py to Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -13,7 +13,50 @@
 # limitations under the License.
 
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+sns.set(style='dark')
 from streamlit.logger import get_logger
+
+byseason_df = pd.read_csv("byseason.csv")
+byseason_df.rename(index={
+    0: "Musim",
+    1: "Spring",
+    2: "Summer",
+    3: "Fall",
+    4: "Winter"
+}, inplace=True)
+byseason_df.rename(columns={
+    "Jumlah Customer.1": "Rata-rata Customer"
+}, inplace=True)
+byseason_df
+
+byweathersit_df = pd.read_csv("byweathersit.csv")
+byweathersit_df.rename(index={
+    0: "Cuaca",
+    1: "Clear",
+    2: "Mist",
+    3: "Light Snow",
+}, inplace=True)
+byweathersit_df.rename(columns={
+    "cnt": "Jumlah Customer",
+    "cnt.1": "Rata-rata Customer"
+}, inplace=True)
+byweathersit_df
+
+byhour_df = pd.read_csv("byhour.csv")
+byhour_df.rename(columns={
+    "casual" : "Casual",
+    "casual.1": "Rata-rata casual",
+    "registered": "Registered",
+    "registered.1": "Rata-rata Registered",
+    "cnt": "Jumlah Customer",
+    "cnt.1": "Rata-rata Customer"
+}, inplace=True)
+byhour_df
 
 LOGGER = get_logger(__name__)
 
@@ -45,7 +88,7 @@ def run():
         - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
     """
     )
-
+st.header('Bike Rental Dashboard 🚲')
 
 if __name__ == "__main__":
     run()
