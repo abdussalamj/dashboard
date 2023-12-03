2676016aea294ae71d461a0a3b588642a922c491
abdussalamj <152305032+abdussalamj@users.noreply.github.com>
2023-12-03 16:08:58 +0700

    Update 3_DataFrame_Demo.py

--- pages/3_DataFrame_Demo.py
+++ pages/3_DataFrame_Demo.py
@@ -124,7 +124,7 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
     daily_rent_casual = daily_rent_df['casual'].sum()
-    st.metric('Casual User', value= daily_rent)
+    st.metric('Casual User', value= daily_rent_df['casual'].sum())
 
 with col2:
     daily_rent_registered = daily_rent_df['registered'].sum()
